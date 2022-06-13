c127b20fdaadebed68931dd240b955f790eccd9e
Vineet Kumar <vk.vempire@gmail.com>
2022-06-13 15:32:30 +0530

    update

--- clientNN/assets/js/learn.js
+++
@@ -1,5 +0,0 @@
-serverUrl = "https://6jvfolc0td4p.usemoralis.com:2053/server";
-appId = 'svwErIclpXy8OOAQESvKnu12putOFhcsSpeEGFSR';
-Moralis.start({ serverUrl, appId });
-
-const user = Moralis.User.current();
